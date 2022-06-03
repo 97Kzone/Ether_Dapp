ddb83b5ba60c4d6b130f671837c835dab8492091
97Kzone <76652908+97Kzone@users.noreply.github.com>
2022-06-04 01:03:08 +0900

    Build. 광고 조회 리스트로 변경

--- contracts/Advertising.sol
+++ contracts/Advertising.sol
@@ -9,59 +9,49 @@ contract Advertising {
         owner = payable(msg.sender);
     }
 
-    // 변수와 매핑
-    uint256 total_adv; // 총 등록된 광고 수 
-
-    mapping(address => uint256) private _addr; // 주소 => 정수
-    mapping(uint256 => AdvInfo) private _advs; // 정수 => 주소
-
     // ver 1 광고는 구조체다
     struct AdvInfo {
-        address advertiser; // 광고주
+        string comment; // 광고내용...초라하다
         uint256 pot; // 광고비
         uint256 count; // 목표 클릭 수.. 인데 금액이 정확하게 1/N 이 안된다면 어쩌지?
     }
 
+    // 변수와 매핑
+    uint256 total_adv; // 총 등록된 광고 수 
+
+    mapping(address => AdvInfo[]) private _advs; //주소 => 구조체 list
+    
+
     //광고 이벤트
-    event AddAdv(address addr, uint256 _pot, uint256 _count);
+    event AddAdv(address addr, uint256 _pot, uint256 _count); // 광고 추가되면 이벤트 발생시켜서 내역 확인
 
     // 광고 등록
-    function addAdv(uint256 _pot, uint256 _count) public payable returns (bool res) {
+    function addAdv(string memory _comment, uint256 _pot, uint256 _count) public payable returns (bool res) {
         require(msg.value == _pot, "Not Enough ETH"); // 잔액이 광고비 만큼 있는지 확인
 
         AdvInfo memory a;
-        a.advertiser = msg.sender;
+
+        a.comment = _comment;
         a.pot = _pot;
         a.count = _count;
         total_adv++;
 
-        uint256 idx = _addr[msg.sender];
-        _advs[idx] = a;
+        _advs[msg.sender].push(a);
         
         emit AddAdv(msg.sender, _pot, _count);
 
         return true;
     }
 
-    // 특정 사용자가 등록한 광고 조회 (생각해보니 한 사람이 여러개의 광고 등록 가능한데 어쩌지 큰일났다)
-    function getMyAdv() public view returns (address advertiser, uint256 pot, uint256 count){
-        AdvInfo memory a = _advs[_addr[msg.sender]];
-
-        advertiser = a.advertiser;
-        pot = a.pot;
-        count = a.count;
+    // 특정 사용자가 등록한 광고 조회 
+    function getMyAdv() public view returns (AdvInfo[] memory res){
+        AdvInfo[] memory a = _advs[msg.sender];
+        return a;
     }
 
-    // 등록된 광고 목록 (내 뜻대로 안됨)
-    function getAllAdv() public view returns (AdvInfo[] memory) {
-        AdvInfo[] memory temp = new AdvInfo[](total_adv);
-
-        for (uint256 i = 0; i < total_adv; i++) {
-            temp[i] = _advs[i];
-        }
-
-        return temp;
-    }   
+    // 등록된 광고 목록 (다음 목표)
+    // function getAllAdv() public view returns (AdvInfo[] memory) {
+    // }   
 
     // 등록된 광고는 수정이 가능해야 할까 ?
 }
